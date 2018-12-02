5c8fabc7a707bdeaebf5161fc0994e74a882881e
Filipe Ferreira <filipeferreira@MBP-de-Filipe.lan>
2018-12-02 21:50:47 +0000

    add structure of website

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>PortFolio Filipe Ferreira</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">My Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Presentação <span class="sr-only">(current)</span></a>
+                </li>
+               
+            </ul>
+            
+            <!-- Modal para efetuar o Login -->
+            <button class="btn btn-success my-2 my-sm-0" type="submit">Login</button>
+            
+        </div>
+    </nav>
+
+    <section id="presentacao">
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="#" alt="Fotografia de Filipe Ferreira">
+                        </div>
+                        <div class="col-md-8">
+                            Estudante e FreeLancer
+                            Estudante no Instituto Politécnico de Viseu
+                            <hr>
+                            Idade
+                            Endereço
+                            E-mail
+                            Estudante
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    Redes Sociais
+                </div>
+            </div>
+            <div class="text-center">
+                <p> 
+                    Viva ! Vou-me apresentar eu sou o Filipe Ferreira fascinado desde à muito tempo para todo o tipo de linguagens 
+                    de programação e de novas tecnologias.
+                </p>
+                <p>
+                    Já a algum tempo que sou autodidata realizando alguns projetos. Um dos meus maiores sucessos:
+                    <a href="https://www.warsofninja.eu" target="_blank" rel="noopener noreferrer">Wars of Ninja</a> 
+                </p>
+            </div>
+        </div>
+    </section>
+    <section id="competencias">
+
+    </section>
+    <section id="projetos">
+
+    </section>
+    <section id="servicos">
+
+    </section>
+    <section id="contatos">
+
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
