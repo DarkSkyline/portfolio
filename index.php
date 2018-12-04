5eb6251ec742a8602d0a0c8fc020d7ef54306d18
Filipe Ferreira <filipeferreira@MacBook-Pro-de-Filipe.local>
2018-12-04 14:26:51 +0000

    add page container

--- index.php
+++ index.php
@@ -30,77 +30,78 @@
             
         </div>
     </nav>
-
-    <section id="presentacao">
-        <div class="container">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-4 col-xs-12">
-                            <img src="img/foto-perfil.jpg"  width="250" alt="Fotografia de Filipe Ferreira">
-                        </div>
-                        <div class="col-md-8 col-xs-12">
-                            <div class="spacing">
-                                <h1><span>Eu sou o </span>Filipe Ferreira</h1>
-                                Estudante e Freelancer
+    <div class="page-container">
+        <section id="presentacao">
+            <div class="container">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-4 col-sm-12">
+                                <img src="img/foto-perfil.jpg" class="img-fluid" width="250" alt="Fotografia de Filipe Ferreira">
                             </div>
-                            <hr>
-                            <table class="table">
-                                <tr>
-                                    <td>Idade</td>
-                                    <td>24</td>
-                                </tr>
-                                <tr>
-                                    <td>Endereço</td>
-                                    <td>Rio de Loba, Viseu</td>
-                                </tr>
-                                <tr>
-                                    <td>E-mail</td>
-                                    <td>filipesferreira94@gmail.com</td>
-                                </tr>
-                                <tr>
-                                    <td>Estudante</td>
-                                    <td>Engenharia Informática (3º ano)</td>
-                                </tr>
-                            </table>
+                            <div class="col-lg-8 col-sm-12">
+                                <div class="spacing">
+                                    <h1><span>Eu sou o </span>Filipe Ferreira</h1>
+                                    Estudante e Freelancer
+                                </div>
+                                <hr>
+                                <table class="table">
+                                    <tr>
+                                        <td class="bold">Idade</td>
+                                        <td>24</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="bold">Endereço</td>
+                                        <td>Rio de Loba, Viseu</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="bold">E-mail</td>
+                                        <td>filipesferreira94 @gmail.com</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="bold">Estudante</td>
+                                        <td>Engenharia Informática (3º ano)</td>
+                                    </tr>
+                                </table>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-footer text-center">
-                    <a href="https://github.com/DarkSkyline" target="_blank"><i class="fab fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/filipesferreira/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                    <a href="https://www.facebook.com/Filipesferreiraa" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                    <a href="https://www.instagram.com/filipesferreira94/" target="_blank"><i class="fab fa-instagram"></i></a>
+                    <div class="card-footer text-center">
+                        <a href="https://github.com/DarkSkyline" target="_blank"><i class="fab fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/filipesferreira/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.facebook.com/Filipesferreiraa" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://www.instagram.com/filipesferreira94/" target="_blank"><i class="fab fa-instagram"></i></a>
 
+                    </div>
+                </div>
+                <div class="descricao text-center">
+                    <p> 
+                        Viva ! Eu sou o Filipe Ferreira, fascinado desde à muito tempo para todo o tipo de linguagens 
+                        de programação e de novas tecnologias.
+                    </p>
+                    <p>
+                        Já a algum tempo que sou autodidata realizando alguns projetos. Um dos meus maiores sucessos:<br>
+                        <a href="https://www.warsofninja.eu" target="_blank" rel="noopener noreferrer"><img src="img/won_logo.png" alt="Logotipo Wars of Ninja" width="300px"></a> 
+                    </p>
                 </div>
             </div>
-            <div class="descricao text-center">
-                <p> 
-                    Viva ! Eu sou o Filipe Ferreira, fascinado desde à muito tempo para todo o tipo de linguagens 
-                    de programação e de novas tecnologias.
-                </p>
-                <p>
-                    Já a algum tempo que sou autodidata realizando alguns projetos. Um dos meus maiores sucessos:<br>
-                    <a href="https://www.warsofninja.eu" target="_blank" rel="noopener noreferrer">Wars of Ninja</a> 
-                </p>
-            </div>
-        </div>
-    </section>
-    <section id="competencias">
+        </section>
+        <section id="competencias">
 
-    </section>
-    <section id="projetos">
+        </section>
+        <section id="projetos">
 
-    </section>
-    <section id="servicos">
+        </section>
+        <section id="servicos">
 
-    </section>
-    <section id="contatos">
+        </section>
+        <section id="contatos">
 
-    </section>
+        </section>
 
+    </div>
     <!-- Footer -->
-    <footer>
+    <footer class="footer">
         <div class="footer-copyright text-center py-3">© 2018 Copyright:
             <a href="https://filipesferreira.com"> filipesferreira.com</a>
         </div>
@@ -108,7 +109,7 @@
     <!-- Footer -->
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
